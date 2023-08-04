411f03c0b7880eb2a52e9a8283aff064a44ac0a2
Manuel-latorre <manuel.latorre11@gmail.com>
2023-08-03 22:47:32 -0300

    users

--- ViewAdmin/src/redux/ProductsActions.js
+++ ViewAdmin/src/redux/ProductsActions.js
@@ -10,20 +10,3 @@ export const getProd = () => async (dispatch) => {
     console.log(error);
   }
 };
-
-/* export const getProdtById = (id: number) => async (dispatch: any) => {
-  try {
-    const response = await axios.get(`http://localhost:3001/products/${id}`);
-    dispatch(getProductById([response.data]));
-  } catch (error) {
-    console.log(error);
-    
-  }
-} */
-
-
-
-/* export const cleanState = ()=>  async (dispatch: any)=>{
-      dispatch(clear());
-}
- */
